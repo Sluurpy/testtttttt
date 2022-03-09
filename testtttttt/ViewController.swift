a4d11d452967f76770a1805672069029127a7622
Sluurpy <info@sluurpy.it>
2022-03-09 10:43:04 +0100

    Initial Commit

---
+++ testtttttt/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  testtttttt
+//
+//  Created by Simone Giovannini on 09/03/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
